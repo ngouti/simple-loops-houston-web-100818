a15ee12132e6ba7dc3bc88cd7d60898499fd8d4b
Emma Gouti <goutiemma@gmail.com>
2018-10-01 02:57:19 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -23,7 +23,7 @@ def while_iterator(number_of_times)
   counter = 0 
   while counter <= number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
-    counter++
+    counter += 1
 
 end
 
